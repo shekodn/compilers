3faac3e5acc248de13e160d562a87fc10e150383
shekodn <sdiazn19@gmail.com>
2019-01-21 00:00:39 -0600

    Implements stack object

--- hw/1/main.go
+++ hw/1/main.go
@@ -2,7 +2,6 @@ package main
 
 import(
   "fmt"
-  "errors"
 )
 
 type stack []int
@@ -11,47 +10,19 @@ func (s stack) Push(x int) stack {
     return append(s, x)
 }
 
-func (s stack) Pop() (stack, int, error) {
-
-    _, err := s.isEmpty()
-    l := len(s)
-
-    if err != nil {
-      fmt.Println(err)
-      return s, 0, err
-    }
-
-    return s[:l-1], s[l-1], nil
-}
-
-func (s stack) isEmpty() (bool, error) {
-  l := len(s)
-
-  if l > 0 {
-    return false, nil
-  }
-
-  return true, errors.New("Error: Stack is empty")
-}
-
-func (s stack) isEmpty2() (bool) {
+func (s stack) isEmpty() (bool) {
   return len(s) == 0
 }
 
-
-
-func (s stack) Pop2() (stack, int) {
+func (s stack) Pop() (stack, int) {
     l := len(s)
 
-    if s.isEmpty2() {
+    if s.isEmpty() {
       return s, 0
     }
 
     return s[:l-1], s[l-1]
 }
-
-
-
 
 func main(){
     myStack := make(stack,0)
@@ -59,16 +30,18 @@ func main(){
     myStack = myStack.Push(2)
     myStack = myStack.Push(3)
 
-    myStack, x, _ := myStack.Pop()
+    myStack, x := myStack.Pop()
     fmt.Println(x)
 
-    myStack, x, _ = myStack.Pop()
+    myStack, x = myStack.Pop() /
     fmt.Println(x)
 
-    myStack, x, _ = myStack.Pop()
+    myStack, x = myStack.Pop()
     fmt.Println(x)
 
-    myStack, x, _ = myStack.Pop()
+    myStack, x = myStack.Pop() //0
     fmt.Println(x)
 
+    myStack, x = myStack.Pop() //0
+    fmt.Println(x)
 }
